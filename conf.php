0e469817d95afeafb9e3ad486492556d91179ae5
AAZ H <aazhbd@yahoo.com>
2018-01-20 15:13:29 +0100

    Updated.

--- conf.php
+++ conf.php
@@ -2,8 +2,19 @@
 
 $conf = array(
     'production' => array(
-        'db_name' => 'prod_db',
-        'development_mode' => false
+        'db_name' => 'ph-start',
+        'db_host' => 'localhost',
+        'db_user' => 'root',
+        'db_pass' => 'root',
+        'development_mode' => true,
+        'path_sys_template' => '/Template/base.twig',
+        'path_static' => '/Template/static/',
+        'path_user_template' => '/App/views',
+        'user_var' => array(
+            'project_name' => 'ArtWebCMS',
+            'project_scripts' => '/App/scripts',
+            'project_static' => '/App/static',
+        ),
     ),
     'development' => array(
         'db_name' => 'ph-start',
